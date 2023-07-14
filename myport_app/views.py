52661fdd845ec1dd0361bf1a4729eb53361485ac
ubuntu <admin@ubuntu.com>
2023-07-14 10:41:29 +0300

    Now

--- myport_app/views.py
+++ myport_app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+# from .models import Service, LatestWork
 
 def main_page(request):
+
+    """
+
+    services = Service.objects.all
+    latest_works = LatestWork.objects.all
+
+    context = {
+        "services": services,
+        "latest_works" : latest_works,
+    }
+
+    """
     return render(request, 'main_page.html')
